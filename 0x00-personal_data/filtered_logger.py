ad3c72f809b2308b3a02dcd3f444a7067fc2e9f8
joyogbu <obyjoy@yahoo.com>
2023-08-05 02:41:08 +0000

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,5 @@ from typing import List, Any, Match
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> Match[str]:
     '''defining the function'''
-    res = re.sub('|'.join('(?<={}=)([^{}]*)'.format(item, separator)
+    return re.sub('|'.join('(?<={}=)([^{}]*)'.format(item, separator)
                           for item in fields), redaction, message)
-    return (res)
