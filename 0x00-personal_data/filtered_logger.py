838d690deae6a0e24da89159265c96a790f1bd6f
joyogbu <obyjoy@yahoo.com>
2023-08-06 19:54:29 +0000

    task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -83,7 +83,7 @@ def get_logger() -> logging.Logger:
     return (logger)
 
 
-def get_db() -> mysql.connector:
+def get_db() -> connection.MySQLConnection:
     '''connect to a database'''
     PERSONAL_DATA_DB_USERNAME = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     PERSONAL_DATA_DB_PASSWORD = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
