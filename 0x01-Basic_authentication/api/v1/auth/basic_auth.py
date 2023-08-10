1d5ddd22aeb7748a481b775890748ca06b05852d
joyogbu <obyjoy@yahoo.com>
2023-08-10 15:13:26 +0000

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     '''Basic Auth that inherits from Auth
     '''
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         '''function to extract base64 of authorization header
         '''
         if authorization_header is None:
