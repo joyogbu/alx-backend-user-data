f8a246207d3ddbc887cefd7a3c347959ff38a867
joyogbu <obyjoy@yahoo.com>
2023-08-12 10:18:21 +0000

    task2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,8 +7,20 @@ from flask import request
 from api.v1.auth.auth import Auth
 from models.user import User
 from typing import TypeVar
+import uuid
 
 
 class SessionAuth(Auth):
     '''class inherits from Auth'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creates a session id for a user id'''
+        if user_id is None:
+            return None
+        if type(user_id) != str:
+            return None
+        sessid = uuid.uuid4()
+        self.user_id_by_session_id[sessid] = user_id
+        return (sessid)
+
