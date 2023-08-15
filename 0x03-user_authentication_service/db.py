d426ae3ad11a91f8e86f120cfa42b0a1623f9036
joyogbu <obyjoy@yahoo.com>
2023-08-15 11:27:52 +0000

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         '''save the user to a database andreturns a user object'''
         new_user = User(email, hashed_password)
         # Base.metadata.create_all(engine)
