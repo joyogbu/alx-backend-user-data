3484d3525e44989a5886f29b0d80c5cab3553020
joyogbu <obyjoy@yahoo.com>
2023-08-16 19:01:19 +0000

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,7 +77,8 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         '''destroy a session'''
         user = self._db.find_user_by(user_id=user_id)
-        user.session_id = None
+        self._db.update_user(user_id, session_id=None)
+        # user.session_id = None
 
 
 def _generate_uuid() -> str:
