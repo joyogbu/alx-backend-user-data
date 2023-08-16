7b0f02c83321651e9a5bfb915b3adddf7c0d6827
joyogbu <obyjoy@yahoo.com>
2023-08-16 19:38:05 +0000

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,7 +80,8 @@ class Auth:
         try:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
-            return
+            pass
+        return None
 
 
 def _generate_uuid() -> str:
