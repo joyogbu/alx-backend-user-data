9afe8b3ea8317c36585ad21b11b1a34d52da42cc
joyogbu <obyjoy@yahoo.com>
2023-08-17 21:48:25 +0000

    task16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,7 +77,7 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         '''find user and destroy the user session'''
         # user = self._db.find_user_by(id=user_id)
-        self._db.update_user(user_id, "session_id": None)
+        self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
         '''get reset password token'''
