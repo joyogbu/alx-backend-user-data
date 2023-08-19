cd013affd4c1fde76206b095e9d49258363341ad
joyogbu <obyjoy@yahoo.com>
2023-08-19 16:40:47 +0000

    edited task 0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -16,7 +16,7 @@ def view_all_users() -> str:
     return jsonify(all_users)
 
 
-@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+# @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
@@ -28,13 +28,13 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+    if user_id == 'me' and request.current_user is not None:
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
-    if user_id == me and request.current_user is None:
-        abort(404)
-    if user_id == me and request.current_user is not None:
-        return jsonify(user.to_json())
     return jsonify(user.to_json())
 
 
